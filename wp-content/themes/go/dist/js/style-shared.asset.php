d54b6ce31a8877cd15667d955b395b8d66a7e87c
Mateusz Szczepaniuk <matszczepaniuk@gmail.com>
2022-11-02 19:32:49 +0100

    wordpress inital config

---
+++ wp-content/themes/go/dist/js/style-shared.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '926b7044380fd99fc505');
---
+++ wp-content/themes/go/dist/js/style-shared.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'bdc8c98a8c86aed2ad6e');
